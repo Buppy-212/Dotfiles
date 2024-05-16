8f25d04372862b3d3179a6f87db3f71707ce2b74
Buppy-212 <buppy212@proton.me>
2024-05-16 11:04:44 +0100

    mm keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,5 +4,5 @@
 
 local map = vim.keymap.set
 map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown Preview" })
-map("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle Markdown Preview" })
+map("n", "<leader>mm", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle Markdown Preview" })
 map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "Stop Markdown Preview" })
