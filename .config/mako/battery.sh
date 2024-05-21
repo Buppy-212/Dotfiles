99efcc675ab76355379366f2af697669636eb7c2
Buppy-212 <buppy212@proton.me>
2024-05-21 13:11:09 +0100

    New batt script

--- .config/mako/battery.sh
+++ .config/mako/battery.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
-battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-if [ $battery_level -le 10 ]
-then
-    notify-send "Battery low" "Battery level is ${battery_level}%!"
+export "$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u "$LOGNAME" gnome-session)/environ | tr '\0' '\n')"
+
+BATTERY_PATH=$(upower -e | grep battery)
+LINE_POWER_PATH=$(upower -e | grep line_power)
+BATTERY_PERCENTAGE=$(upower -i "$BATTERY_PATH" | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//')
+CABLE_PLUGGED=$(upower -i "$LINE_POWER_PATH" | grep -A2 'line-power' | grep online | awk '{ print $2 }')
+
+if [[ $CABLE_PLUGGED == 'yes' ]]; then
+
+    if [[ "$BATTERY_PERCENTAGE" -gt 80 ]]; then
+        notify-send --urgency=critical "Battery reached 80%" "Charging has stopped"
+    fi
+
+else
+
+    if [[ "$BATTERY_PERCENTAGE" -lt 10 ]]; then
+        notify-send --urgency=critical "Battery is below 10%" "Plug in the power cable"
+    fi
+
 fi
+
