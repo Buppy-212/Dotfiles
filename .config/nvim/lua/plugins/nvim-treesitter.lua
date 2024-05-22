a279d74e02fa7ed0013665b6a0a4252fc5c8edad
Buppy-212 <buppy212@proton.me>
2024-05-22 18:24:51 +0100

    treesitter enables automatically now

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = "VeryLazy",
-  opts = {},
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn", -- set to `false` to disable one of the mappings
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
 }
