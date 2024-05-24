e2b5c900745326d48ace0b26fe2ee982a275c148
Buppy-212 <buppy212@proton.me>
2024-05-24 02:20:23 +0100

    tree sitter on enter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPre",
+  event = "VimEnter",
   config = function()
     require("nvim-treesitter.configs").setup({
       highlight = {
