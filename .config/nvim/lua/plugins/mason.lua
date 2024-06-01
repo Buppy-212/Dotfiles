bbce8490a7ba5d59d1a0dc7a6398699531bb95cf
Buppy-212 <buppy212@proton.me>
2024-06-01 01:58:45 +0100

    Snippet completion for weird lsps

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -11,9 +11,15 @@ return {
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup()
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
     require("mason-lspconfig").setup_handlers({
       function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup({})
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+        })
       end,
       vim.keymap.set("n", "<leader>cm", "<cmd>Mason<cr>", { desc = "Mason" }),
     })
@@ -24,6 +30,7 @@ return {
         },
       },
     })
+
     vim.keymap.set("n", "<leader>cr", "<cmd> lua vim.lsp.buf.rename()<cr>", { desc = "Rename" })
     vim.keymap.set("n", "<leader>cd", "<cmd> lua vim.lsp.buf.definition()<cr>", { desc = "Definition" })
     vim.keymap.set("n", "<leader>ca", "<cmd> lua vim.lsp.buf.code_action()<cr>", { desc = "Action" })
