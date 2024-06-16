bd6661490e1ad073a40c9495b0afaf1d89c9c4b4
Buppy-212 <buppy212@proton.me>
2024-06-16 03:02:19 +0100

    task deleter

--- .config/wofi/scripts/task-switcher.sh
+++ .config/wofi/scripts/task-switcher.sh
@@ -3,17 +3,18 @@
 # Icon Theme
 ICONDIR=$HOME/.local/share/icons/BeautyLine/apps/scalable/
 
-# Get Task Info
-readarray -t CLASS < <(hyprctl clients | grep class: | cut -d" " -f 2)
-readarray -t WORKSPACE < <(hyprctl clients | grep workspace: | cut -d" " -f 2)
-readarray -t ADDR < <(hyprctl clients | grep Window | cut -d" " -f 2)
-readarray -t TITLE < <(hyprctl clients | grep title: | sed "s/title: //g")
-
 # Wofi Command
 CONFIG="$HOME/.config/wofi/taskconfig"
 STYLE="$HOME/.config/wofi/style.css"
+WOFI_COMMAND="wofi  --conf $CONFIG --style $STYLE"
 
-wofi_command="wofi  --conf $CONFIG --style $STYLE"
+# Get Task Info
+tasks(){
+  readarray -t CLASS < <(hyprctl clients | grep class: | cut -d" " -f 2)
+  readarray -t WORKSPACE < <(hyprctl clients | grep workspace: | cut -d" " -f 2)
+  readarray -t ADDR < <(hyprctl clients | grep Window | cut -d" " -f 2)
+  readarray -t TITLE < <(hyprctl clients | grep title: | sed "s/title: //g")
+}
 
 # Image and text for wofi
 menu() {
@@ -22,16 +23,32 @@ menu() {
 	done
 }
 
-main() {
-	# Wofi output
-	choice=$(menu | ${wofi_command})
-	workspace=$( echo "$choice" | cut -f 3 )
-	window=$( echo "$choice" | cut -f 4)
+main(){
+  tasks
 
-	# Make choice the active task
-	hyprctl dispatch focusworkspaceoncurrentmonitor "$workspace"
-	hyprctl dispatch focuswindow address:0x"$window"
-	hyprctl dispatch bringactivetotop address:0x"$window"
+  # Wofi output
+  choice=$(menu | ${WOFI_COMMAND})
+  workspace=$( echo "$choice" | cut -f 3 )
+  window=$( echo "$choice" | cut -f 4)
+
+  # Break condition
+  if [ -z "$choice" ]; then
+    return
+  fi
+
+  # Make choice the active task
+  if [ "$1" = "switch" ]; then
+    hyprctl dispatch focusworkspaceoncurrentmonitor "$workspace"
+    hyprctl dispatch focuswindow address:0x"$window"
+    hyprctl dispatch bringactivetotop address:0x"$window"
+  fi
+  
+  # Kill the window
+  if [ "$1" = "kill" ]; then
+    hyprctl dispatch closewindow address:0x"$window"
+    sleep 0.1 # So the window fully closes
+    main "$1"
+  fi
 }
 
 # Kill wofi if it is already running
@@ -39,5 +56,5 @@ if pidof wofi >/dev/null; then
 	killall wofi
 	exit 0
 else
-	main
+  main "$1"
 fi
