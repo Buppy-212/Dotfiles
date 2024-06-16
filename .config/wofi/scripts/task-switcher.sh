072b33582a8e9810fbf58c63214b347baa4e058f
Buppy-212 <buppy212@proton.me>
2024-06-16 02:26:18 +0100

    task-switcher brings to current monitor

--- .config/wofi/scripts/task-switcher.sh
+++ .config/wofi/scripts/task-switcher.sh
@@ -5,6 +5,7 @@ ICONDIR=$HOME/.local/share/icons/BeautyLine/apps/scalable/
 
 # Get Task Info
 readarray -t CLASS < <(hyprctl clients | grep class: | cut -d" " -f 2)
+readarray -t WORKSPACE < <(hyprctl clients | grep workspace: | cut -d" " -f 2)
 readarray -t ADDR < <(hyprctl clients | grep Window | cut -d" " -f 2)
 readarray -t TITLE < <(hyprctl clients | grep title: | sed "s/title: //g")
 
@@ -17,17 +18,20 @@ wofi_command="wofi  --conf $CONFIG --style $STYLE"
 # Image and text for wofi
 menu() {
 	for i in "${!ADDR[@]}"; do
-		printf "img:%s%s.svg:text:%s\t%s\n" "$ICONDIR" "${CLASS[$i],,}" "${TITLE[$i]}" "${ADDR[$i]}"
+		printf "img:%s%s.svg:text:%s\t%s\t%s\n" "$ICONDIR" "${CLASS[$i],,}" "${TITLE[$i]}" "${WORKSPACE[$i]}" "${ADDR[$i]}"
 	done
 }
 
 main() {
 	# Wofi output
-	choice=$(menu | ${wofi_command} | cut -f 3)
+	choice=$(menu | ${wofi_command})
+	workspace=$( echo "$choice" | cut -f 3 )
+	window=$( echo "$choice" | cut -f 4)
 
 	# Make choice the active task
-	hyprctl dispatch focuswindow address:0x"$choice"
-	hyprctl dispatch bringactivetotop address:0x"$choice"
+	hyprctl dispatch focusworkspaceoncurrentmonitor "$workspace"
+	hyprctl dispatch focuswindow address:0x"$window"
+	hyprctl dispatch bringactivetotop address:0x"$window"
 }
 
 # Kill wofi if it is already running
