12fd3211eca10c9c48cf619d3a18aa64f1ee08fd
Buppy-212 <buppy212@proton.me>
2024-06-16 03:57:06 +0100

    delete stops when all windows are deleted

--- .config/wofi/scripts/task-switcher.sh
+++ .config/wofi/scripts/task-switcher.sh
@@ -10,42 +10,45 @@ WOFI_COMMAND="wofi  --conf $CONFIG --style $STYLE"
 
 # Get Task Info
 tasks(){
-  readarray -t CLASS < <(hyprctl clients | grep class: | cut -d" " -f 2)
-  readarray -t WORKSPACE < <(hyprctl clients | grep workspace: | cut -d" " -f 2)
-  readarray -t ADDR < <(hyprctl clients | grep Window | cut -d" " -f 2)
-  readarray -t TITLE < <(hyprctl clients | grep title: | sed "s/title: //g")
+  readarray -t CLASS < <(hyprctl clients | awk 'NR%23==9  {print $2}')
+  readarray -t WORKSPACE < <(hyprctl clients | awk 'NR%23==6  {print $2}')
+  readarray -t ADDR < <(hyprctl clients | awk 'NR%23==1  {print $2}')
+  readarray -t TITLE < <(hyprctl clients | awk 'NR%23==10 {s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s}')
 }
 
 # Image and text for wofi
 menu() {
 	for i in "${!ADDR[@]}"; do
-		printf "img:%s%s.svg:text:%s\t%s\t%s\n" "$ICONDIR" "${CLASS[$i],,}" "${TITLE[$i]}" "${WORKSPACE[$i]}" "${ADDR[$i]}"
+		printf "img:%s%s.svg:text:\t%s\t%s\t%s\n" "$ICONDIR" "${CLASS[$i],,}" "${TITLE[$i]}" "${WORKSPACE[$i]}" "${ADDR[$i]}"
 	done
 }
 
 main(){
   tasks
+  if [ "${ADDR[0]}" = "request" ]; then
+    return
+  fi
 
   # Wofi output
-  choice=$(menu | ${WOFI_COMMAND})
-  workspace=$( echo "$choice" | cut -f 3 )
-  window=$( echo "$choice" | cut -f 4)
+  CHOICE=$(menu | ${WOFI_COMMAND})
+  SPACE=$( echo "$CHOICE" | cut -f 3 )
+  WINDOW=$( echo "$CHOICE" | cut -f 4)
 
   # Break condition
-  if [ -z "$choice" ]; then
+  if [ -z "$CHOICE" ]; then
     return
   fi
 
   # Make choice the active task
   if [ "$1" = "switch" ]; then
-    hyprctl dispatch focusworkspaceoncurrentmonitor "$workspace"
-    hyprctl dispatch focuswindow address:0x"$window"
-    hyprctl dispatch bringactivetotop address:0x"$window"
+    hyprctl dispatch focusworkspaceoncurrentmonitor "$SPACE"
+    hyprctl dispatch focuswindow address:0x"$WINDOW"
+    hyprctl dispatch bringactivetotop address:0x"$WINDOW"
   fi
   
   # Kill the window
   if [ "$1" = "kill" ]; then
-    hyprctl dispatch closewindow address:0x"$window"
+    hyprctl dispatch closewindow address:0x"$WINDOW"
     sleep 0.1 # So the window fully closes
     main "$1"
   fi
