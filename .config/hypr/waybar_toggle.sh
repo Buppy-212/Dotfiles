b3bcbaf398455ffb324ec5a0fae2cb3eebea58f7
Buppy-212 <buppy212@proton.me>
2024-07-20 21:59:54 +0100

    waybar top/left toggle

--- .config/hypr/waybar_toggle.sh
+++ .config/hypr/waybar_toggle.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-if pidof waybar; then
+ROUNDING=$(hyprctl getoption decoration:rounding | awk 'NR==1 {print $2 }')
+if [ "$ROUNDING" -eq 3 ]; then
   hyprctl --batch "keyword general:gaps_in 0 ; keyword general:gaps_out 0 ; keyword general:border_size 0 ; keyword decoration:rounding 0"
+  killall waybar
+  waybar --config "$HOME"/.config/waybar/config_top.jsonc --style "$HOME"/.config/waybar/style_top.css
+elif pidof waybar; then
   killall waybar
 else
   hyprctl --batch "keyword general:gaps_in 2 ; keyword general:gaps_out 4 ; keyword general:border_size 2 ; keyword decoration:rounding 3"
-  waybar 
+  waybar --config "$HOME"/.config/waybar/config.jsonc --style "$HOME"/.config/waybar/style.css
 fi
