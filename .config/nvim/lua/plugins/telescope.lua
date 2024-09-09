4beef0dff0c0b4b0832f453f9e87095e558ed571
Buppy-212 <buppy212@proton.me>
2024-09-09 10:12:40 +0100

    Nvim: Fixed undo telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -36,7 +36,7 @@ return {
       { desc = "Hidden files" }
     )
     require("telescope").load_extension("undo")
-    vim.keymap.set("n", "<leader>fu", "<cmd>Undo<cr>")
+    vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>", { desc = "Undo" })
   end,
   dependencies = {
     "nvim-lua/plenary.nvim",
