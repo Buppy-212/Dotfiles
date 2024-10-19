927dce5654a40bd58686c6c2da6d52daf7ce3db3
Buppy-212 <buppy212@proton.me>
2024-10-19 07:24:50 +0100

    Snip

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -28,7 +28,7 @@ return {
       exclude = { "tex" },
     })
     luasnip.config.setup()
-    require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/lua/snippets/" })
 
     cmp.setup({
       completion = {
