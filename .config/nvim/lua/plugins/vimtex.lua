f4cf193dffe7e3f1fc0a9479a436dba27f771aaf
Buppy-212 <buppy212@proton.me>
2024-11-19 13:59:38 +0000

    Vimtex: zathura simple
    wayland moment

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
   ft = { "tex" },
   init = function()
     vim.g.tex_flavor = "latex"
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "zathura_simple"
     vim.g.vimtex_indent_enabled = 0
     vim.g.vimtex_imaps_enabled = 0
     vim.g.vimtex_completion_enabled = 0
