76e99962b2e776d543402c8fa316cc694e0b5f1f
Buppy-212 <buppy212@proton.me>
2024-11-20 16:03:11 +0000

    Kitty border 0

--- .config/nvim/lua/lazy/keymaps.lua
+++ .config/nvim/lua/lazy/keymaps.lua
@@ -9,6 +9,15 @@ map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>bl", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>bh", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+map("n", "<leader>bo", function()
+  local bufs = vim.api.nvim_list_bufs()
+  local current_buf = vim.api.nvim_get_current_buf()
+  for _, i in ipairs(bufs) do
+    if i ~= current_buf then
+      vim.api.nvim_buf_delete(i, {})
+    end
+  end
+end, { desc = "Delete other buffers" })
 
 -- Clear Search
 map("n", "<C-l>", "<cmd>noh<cr>", { desc = "Clear search" })
--- .config/nvim/lua/plugins/terminal.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  lazy = true,
-  keys = {
-    { "<leader>t", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "Terminal" },
-  },
-  opts = { persist_mode = false, start_in_insert = true },
-}
