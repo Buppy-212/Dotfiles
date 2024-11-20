308f85d308fb47c9b626d54e7557481a78a8fa66
Buppy-212 <buppy212@proton.me>
2024-11-20 14:17:50 +0000

    Nvim: Removed debugger and changed yanky keybinds

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,8 +3,6 @@ return {
   dependencies = {
     "neovim/nvim-lspconfig",
     "williamboman/mason-lspconfig",
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
     "mfussenegger/nvim-lint",
     "mhartington/formatter.nvim",
   },
