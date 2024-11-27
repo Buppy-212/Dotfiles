2c16c9f89b5467e46f7d9f0ee3b9975e645cb091
Buppy-212 <buppy212@proton.me>
2024-11-27 23:07:12 +0000

    send latexindent log file to /dev/null

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        tex = { "latexindent" },
+        tex = { "latexindent -c /dev/null" },
         bib = { "bibtex-tidy" },
         markdown = { "markdownlint" },
         sh = { "shfmt" },
