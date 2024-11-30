44bb852d660bbe703a5cd9662b6aa2ded268ffb0
Buppy-212 <buppy212@proton.me>
2024-11-30 12:05:48 +0000

    Media keys alternative

--- .config/rofi/scripts/music.sh
+++ .config/rofi/scripts/music.sh
@@ -2,7 +2,12 @@
 MUSIC=$HOME/Music/Playlist/
 case "$1" in
 "")
+  echo "--Shuffle All--"
   find "$MUSIC" -mindepth 1 | cut -d / -f 1-5 --complement
+  ;;
+"--Shuffle All--")
+  pkill mpv
+  coproc mpv --shuffle "$MUSIC"
   ;;
 *)
   pkill mpv
