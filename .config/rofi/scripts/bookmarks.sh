3700e785ff43763a3e1459d1227494e6b71138db
Buppy-212 <buppy212@proton.me>
2024-12-11 20:40:01 +0000

    bookmarks script new window

--- .config/rofi/scripts/bookmarks.sh
+++ .config/rofi/scripts/bookmarks.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+BROWSER=zen-browser
 BOOKMARKS=$HOME/.local/state/bookmarks.txt
 case "$1" in
 "")
   cat "$BOOKMARKS" || touch "$BOOKMARKS"
   ;;
+"Window")
+  coproc $BROWSER -new-window "$(cat /tmp/bookmark)"
+  ;;
+"Tab")
+  coproc $BROWSER "$(cat /tmp/bookmark)"
+  ;;
 *)
   if grep -q -e "^$1$" "$BOOKMARKS"; then
-    coproc xdg-open "$1"
+    echo "Tab"
+    echo "Window"
+    echo "$1" >/tmp/bookmark
   else
     echo "$1" >>"$BOOKMARKS"
   fi
