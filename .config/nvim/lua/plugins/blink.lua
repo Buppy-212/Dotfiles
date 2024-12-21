118903ac7af0ccb58336f84ea1610eff1244349f
Buppy-212 <buppy212@proton.me>
2024-12-21 05:05:12 +0000

    blink.cmp: luasnips integration

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,9 +2,40 @@ return {
   "saghen/blink.cmp",
   event = "InsertEnter",
   dependencies = {
-    "rafamadriz/friendly-snippets",
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load({ exclude = "tex" })
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      keys = {
+        {
+          "<C-J>",
+          mode = { "i", "s" },
+          function()
+            if require("luasnip").choice_active() then
+              require("luasnip").change_choice(1)
+            end
+          end,
+        },
+        {
+          "<C-K>",
+          mode = { "i", "s" },
+          function()
+            if require("luasnip").choice_active() then
+              require("luasnip").change_choice(-1)
+            end
+          end,
+        },
+      },
+    },
   },
   version = "v0.*",
+
   opts = {
     keymap = {
       preset = "default",
@@ -26,14 +57,25 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
     sources = {
-      default = { "lsp", "path", "snippets" },
+      default = { "lsp", "path", "luasnip" },
       providers = {
-        snippets = {
+        luasnip = {
           min_keyword_length = 1,
-          opts = {
-            friendly_snippets = true,
-          },
         },
       },
     },
