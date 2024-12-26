a44c114691fd79dd57731897bf24d31cc378bf1e
Buppy-212 <buppy212@proton.me>
2024-12-26 08:32:40 +0000

    Yazi fix

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   lazy = true,
   init = function()
     vim.wo.foldmethod = "expr"
