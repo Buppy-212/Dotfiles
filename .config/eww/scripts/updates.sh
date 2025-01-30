0e1befa01ca12427a57d04af7e70b59c4de8af60
Buppy-212 <buppy212@proton.me>
2025-01-30 12:17:40 +0000

    eww

---
+++ .config/eww/scripts/updates.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env dash
+
+case "$1" in
+--update)
+  ghostty --title=Update -e 'yay -Syu; flatpak update'
+  echo "0" >"$HOME"/.local/state/updates
+  eww update updates="{\"tooltip\":\"\",\"text\":\"\"}"
+  eww reload
+  ;;
+--check)
+  AUR=$(yay -Qua | wc -l)
+  OFFICIAL=$(checkupdates | wc -l)
+  FLATPAK=$(flatpak remote-ls --updates | wc -l)
+  echo $((OFFICIAL + AUR + FLATPAK))
+  ;;
+*)
+  COUNT=$(cat "$HOME"/.local/state/updates)
+  if test "$COUNT" = "0"; then
+    printf "{\"tooltip\":\"\",\"text\":\"\"}"
+  else
+    printf "{\"tooltip\":\"%s\",\"text\":\" \"}" "$COUNT"
+  fi
+  exit 0
+  ;;
+esac
