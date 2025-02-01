de08e67b65389c9a1dedf9ed05d54aa7031d28fc
Buppy-212 <buppy212@proton.me>
2025-02-01 11:57:14 +0000

    eww (tweak): poll intervals changed

--- .config/eww/scripts/updates.sh
+++ .config/eww/scripts/updates.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env dash
 
-case "$1" in
---update)
-  ghostty --title=Update -e 'yay -Syu; flatpak update'
-  echo "0" >"$HOME"/.local/state/updates
-  eww update updates="{\"tooltip\":\"Updated\",\"text\":\" \"}"
-  ;;
---check)
+check() {
   AUR=$(yay -Qua | wc -l)
   OFFICIAL=$(checkupdates | wc -l)
   FLATPAK=$(flatpak remote-ls --updates | wc -l)
   echo $((OFFICIAL + AUR + FLATPAK)) >"$HOME"/.local/state/updates
-  ;;
-*)
+  eww update updates="$(display)"
+}
+
+display() {
   COUNT=$(cat "$HOME"/.local/state/updates)
   if [ "$COUNT" = "0" ]; then
     printf "{\"tooltip\":\"Updated\",\"text\":\" \"}"
   else
     printf "{\"tooltip\":\"%s\",\"text\":\" \"}" "$COUNT"
   fi
-  exit 0
+}
+
+case "$1" in
+--update)
+  ghostty --title=update -e 'yay -Syu; flatpak update'
+  check
+  ;;
+--check)
+  check
+  ;;
+*)
+  display
   ;;
 esac
