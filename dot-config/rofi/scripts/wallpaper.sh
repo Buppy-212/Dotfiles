b2310941c41e70fd0c739d85162e1c241f71a554
Buppy-212 <buppy212@proton.me>
2025-02-19 12:17:37 +0000

    rofi: background

--- dot-config/rofi/scripts/wallpaper.sh
+++ dot-config/rofi/scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR="$HOME"/Pictures/Wallpapers/
 get() {
-  readarray -t PICS < <(find "$DIR" | grep -e ".png$" -e ".jpg$" -e ".jpeg$" -e ".webp$")
+  readarray -t PICS < <(find "$DIR" -maxdepth 1 | grep -e ".png$" -e ".jpg$" -e ".jpeg$" -e ".webp$")
   for a in "${PICS[@]}"; do
     name=$(basename "$a")
     echo -en "$name\0icon\x1f$a\n"
