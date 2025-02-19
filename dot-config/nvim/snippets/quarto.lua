3ce65a9d692b4f0b568c0d67ab44a39d80fd59b1
Buppy-212 <buppy212@proton.me>
2025-02-19 15:40:03 +0000

    rofi: bigger button icons

--- dot-config/nvim/snippets/quarto.lua
+++ dot-config/nvim/snippets/quarto.lua
@@ -7,22 +7,24 @@ local postfix = require("luasnip.extras.postfix").postfix
 local l = require("luasnip.extras").lambda
 
 return {
-	postfix({ trig = ";;", snippetType = "autosnippet" }, { l("\\" .. l.POSTFIX_MATCH .. "") }),
-	postfix(
-		{ trig = ";ov", snippetType = "autosnippet" },
-		{ l("\\overset{" .. l.POSTFIX_MATCH .. "}{"), i(1), t("}"), i(0) }
-	),
-	postfix({ trig = ";bar", snippetType = "autosnippet" }, { l("\\bar{" .. l.POSTFIX_MATCH .. "}"), i(0) }),
-	postfix({ trig = ";hat", snippetType = "autosnippet" }, { l("\\hat{" .. l.POSTFIX_MATCH .. "}"), i(0) }),
-	postfix({ trig = ";c", snippetType = "autosnippet" }, { l("\\" .. l.POSTFIX_MATCH .. "{"), i(1), t("}"), i(0) }),
-	postfix(
-		{ trig = ";f", snippetType = "autosnippet" },
-		{ l("\\frac{" .. l.POSTFIX_MATCH .. "}{"), i(1), t("}"), i(0) }
-	),
-	s(
-		"meta",
-		fmt(
-			[[
+  postfix({ trig = ";;", snippetType = "autosnippet" }, { l("\\" .. l.POSTFIX_MATCH .. "") }),
+  postfix(
+    { trig = ";o", snippetType = "autosnippet" },
+    { l("\\overset{" .. l.POSTFIX_MATCH .. "}{"), i(1), t("}"), i(0) }
+  ),
+  postfix({ trig = ";b", snippetType = "autosnippet" }, { l("\\bar{" .. l.POSTFIX_MATCH .. "}"), i(0) }),
+  postfix({ trig = ";h", snippetType = "autosnippet" }, { l("\\hat{" .. l.POSTFIX_MATCH .. "}"), i(0) }),
+  postfix({ trig = ";c", snippetType = "autosnippet" }, { l("\\" .. l.POSTFIX_MATCH .. "{"), i(1), t("}"), i(0) }),
+  postfix({ trig = ";s", snippetType = "autosnippet" }, { l("" .. l.POSTFIX_MATCH .. "_{"), i(1), t("}"), i(0) }),
+  postfix({ trig = ";e", snippetType = "autosnippet" }, { l("" .. l.POSTFIX_MATCH .. "^{"), i(1), t("}"), i(0) }),
+  postfix(
+    { trig = "//", snippetType = "autosnippet" },
+    { l("\\frac{" .. l.POSTFIX_MATCH .. "}{"), i(1), t("}"), i(0) }
+  ),
+  s(
+    "meta",
+    fmt(
+      [[
     ---
     title: "{}"
     author: Will
@@ -44,57 +46,57 @@ return {
     ---
     {}
   ]],
-			{ i(1), i(0) }
-		)
-	),
-	s(
-		"ma",
-		fmt(
-			[[
+      { i(1), i(0) }
+    )
+  ),
+  s(
+    "ma",
+    fmt(
+      [[
   ${}$
   ]],
-			{ i(0) }
-		)
-	),
-	s(
-		"eq",
-		fmt(
-			[[
+      { i(0) }
+    )
+  ),
+  s(
+    "eq",
+    fmt(
+      [[
   $$
     {} 
   $$
   ]],
-			{ i(0) }
-		)
-	),
-	s(
-		"fr",
-		fmt(
-			[[
-      \frac{<>}{<>} <>
+      { i(0) }
+    )
+  ),
+  s(
+    "fr",
+    fmt(
+      [[
+      \frac{<>}{<>}<>
   ]],
-			{ i(1), i(2), i(0) },
-			{ delimiters = "<>" }
-		)
-	),
-	s(
-		{ trig = ";e", snippetType = "autosnippet" },
-		fmt(
-			[[
-      ^{<>} <>
+      { i(1), i(2), i(0) },
+      { delimiters = "<>" }
+    )
+  ),
+  s(
+    { trig = ";e", snippetType = "autosnippet" },
+    fmt(
+      [[
+      ^{<>}<>
     ]],
-			{ i(1), i(0) },
-			{ delimiters = "<>" }
-		)
-	),
-	s(
-		{ trig = ";s", snippetType = "autosnippet" },
-		fmt(
-			[[
-      _{<>} <>
+      { i(1), i(0) },
+      { delimiters = "<>" }
+    )
+  ),
+  s(
+    { trig = ";s", snippetType = "autosnippet" },
+    fmt(
+      [[
+      _{<>}<>
     ]],
-			{ i(1), i(0) },
-			{ delimiters = "<>" }
-		)
-	),
+      { i(1), i(0) },
+      { delimiters = "<>" }
+    )
+  ),
 }
