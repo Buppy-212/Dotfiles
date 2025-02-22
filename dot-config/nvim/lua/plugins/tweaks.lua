e184c5d9c022db51febb04cbd087b7a2470f6723
Buppy-212 <buppy212@proton.me>
2025-02-22 09:52:02 +0000

    ghostty: control+enter = enter

--- dot-config/nvim/lua/plugins/tweaks.lua
+++ dot-config/nvim/lua/plugins/tweaks.lua
@@ -1,53 +1,56 @@
 return {
-	{
-		"folke/snacks.nvim",
-		opts = {
-			image = {
-				enabled = true,
-			},
-			statuscolumn = { enabled = true },
-			picker = {
-				win = {
-					input = {
-						keys = {
-							["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
-						},
-					},
-				},
-			},
-		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		ft = { "markdown", "norg", "rmd", "org", "codecompanion", "quarto" },
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				tex = { "latexindent" },
-			},
-			formatters = {
-				latexindent = {
-					args = { "-g", "/dev/null" },
-				},
-			},
-		},
-	},
-	{
-		"R-nvim/R.nvim",
-		opts = {
-			pdfviewer = "zathura",
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+        math = {
+          latex = { font_size = "Small" },
+        },
+      },
+      statuscolumn = { enabled = true },
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion", "quarto" },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        tex = { "latexindent" },
+      },
+      formatters = {
+        latexindent = {
+          args = { "-g", "/dev/null" },
+        },
+      },
+    },
+  },
+  {
+    "R-nvim/R.nvim",
+    opts = {
+      pdfviewer = "zathura",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
