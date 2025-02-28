8bf57abb94ce00eedf828e22741eff36105ebeb5
Buppy-212 <buppy212@proton.me>
2025-02-28 05:11:13 +0000

    nvim: disabled buffer completion source

--- dot-config/nvim/lua/plugins/blink.lua
+++ dot-config/nvim/lua/plugins/blink.lua
@@ -1,57 +1,58 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = {
-		{
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip").setup({ enable_autosnippets = true })
-				require("luasnip.loaders.from_vscode").lazy_load({ exclude = { "tex" } })
-				require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-			end,
-		},
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			keys = {
-				{
-					"<C-J>",
-					mode = { "i", "s" },
-					function()
-						if require("luasnip").choice_active() then
-							require("luasnip").change_choice(1)
-						end
-					end,
-				},
-				{
-					"<C-K>",
-					mode = { "i", "s" },
-					function()
-						if require("luasnip").choice_active() then
-							require("luasnip").change_choice(-1)
-						end
-					end,
-				},
-			},
-		},
-	},
-	opts = {
-		keymap = {
-			preset = "default",
-			["<C-space>"] = { "show", "select_and_accept" },
-			["<Tab>"] = { "select_next", "fallback" },
-			["<S-Tab>"] = { "select_prev", "fallback" },
-			["<C-l>"] = { "snippet_forward" },
-			["<C-h>"] = { "snippet_backward" },
-			["<C-k>"] = { "show_documentation", "hide_documentation", "fallback" },
-			["<C-d>"] = { "scroll_documentation_down" },
-			["<C-u>"] = { "scroll_documentation_up" },
-		},
-		sources = {
-			providers = {
-				snippets = {
-					min_keyword_length = 1,
-				},
-			},
-		},
-	},
+  "saghen/blink.cmp",
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip").setup({ enable_autosnippets = true })
+        require("luasnip.loaders.from_vscode").lazy_load({ exclude = { "tex" } })
+        require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      keys = {
+        {
+          "<C-J>",
+          mode = { "i", "s" },
+          function()
+            if require("luasnip").choice_active() then
+              require("luasnip").change_choice(1)
+            end
+          end,
+        },
+        {
+          "<C-K>",
+          mode = { "i", "s" },
+          function()
+            if require("luasnip").choice_active() then
+              require("luasnip").change_choice(-1)
+            end
+          end,
+        },
+      },
+    },
+  },
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<C-space>"] = { "show", "select_and_accept" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-l>"] = { "snippet_forward" },
+      ["<C-h>"] = { "snippet_backward" },
+      ["<C-k>"] = { "show_documentation", "hide_documentation", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down" },
+      ["<C-u>"] = { "scroll_documentation_up" },
+    },
+    sources = {
+      providers = {
+        buffer = { enabled = false },
+        snippets = {
+          min_keyword_length = 1,
+        },
+      },
+    },
+  },
 }
