d43db3e96bc3f4eba96cab9e3ef98048cf1c3b31
Buppy-212 <buppy212@proton.me>
2025-03-31 19:43:46 +0100

    eww: volume notifications

--- dot-config/eww/scripts/volume.sh
+++ dot-config/eww/scripts/volume.sh
@@ -15,14 +15,41 @@ getVolume() {
   printf "{\"volume\":\"%s\",\"icon\":\"%s\"}" "$volume" "$icon"
 }
 
+iDIR="$HOME/.config/mako/icons"
+
+get_volume() {
+  volume=$(pamixer --get-volume)
+  echo "$volume"
+}
+
+get_icon() {
+  current=$(get_volume)
+  if [ "$current" -eq "0" ]; then
+    echo "$iDIR/volume-mute.png"
+  elif [ "$current" -ge "0" ] && [ "$current" -le "30" ]; then
+    echo "$iDIR/volume-low.png"
+  elif [ "$current" -ge "30" ] && [ "$current" -le "60" ]; then
+    echo "$iDIR/volume-mid.png"
+  elif [ "$current" -ge "60" ] && [ "$current" -le "100" ]; then
+    echo "$iDIR/volume-high.png"
+  fi
+}
+
 case "$1" in
 "up")
   pamixer -i 5
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Volume : $(get_volume) %"
   ;;
 "down")
   pamixer -d 5
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Volume : $(get_volume) %"
+  ;;
+"")
+  getVolume
   ;;
 *)
+  pamixer --set-volume "$1"
   getVolume
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Volume : $(get_volume) %"
   ;;
 esac
